f979d66437acf02ddb29aaa7dda5631df0552860
Trisha Minela <82831038+TrishaMinela@users.noreply.github.com>
2024-11-03 00:37:44 +0000

    Calling Node BackEnd from Front End

--- client/react-tvshow-app/src/App.js
+++ client/react-tvshow-app/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
+//useState : current state value, function to update the value
+//useEffect : Reacthook for side effects in function components
+import { useState, useEffect } from 'react';
+
 function App() {
+
+  //questions: initial value
+  //setQuestions: function to update the value
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [quizStarted, setQuizStarted] = useState(false);
+  const [quizFinish, setQuizFinishes] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  //Calling Node Backend API
+  useEffect(() => {
+    fetch("http://localhost:5000/api/questions")
+      .then(res => res.json())
+      .then(data => {
+        setQuestions(data.questions)
+        setLoading(false)
+      })
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
